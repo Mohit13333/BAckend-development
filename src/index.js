a911f2ec75732b7e786b685244db6c4498e8feb2
Mohit13333 <mohitkumar143680@gmail.com>
2024-09-14 22:15:07 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+// import {app} from "./app.js"
+import app from "./app.js"
 
 
 dotenv.config({
@@ -9,7 +11,15 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Srever is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failled !!!",err);
+})
 
 
 
