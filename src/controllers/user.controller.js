3d774981c36e15d5d70b352fc9d81e6878ddfe6f
Mohit13333 <mohitkumar143680@gmail.com>
2024-09-18 12:34:01 +0530

    new

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import uploadOnCloudinary from "../utils/cloudinary.js";
 import ApiRespone from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
 import ApiResponse from '../utils/ApiResponse.js';
+import mongoose from "mongoose"
 
 const generateAccessAndRefreshToken =async (userId) => {
   try {
@@ -160,8 +161,8 @@ const logoutUser=asyncHandler(async(req,res)=>{
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set:{
-        refreshToken:undefined
+      $unset:{
+        refreshToken:1 //this remove the file from the document
       }
     },
     {
@@ -375,7 +376,7 @@ const getWatchHistory = asyncHandler(async(req, res) => {
   const user = await User.aggregate([
       {
           $match: {
-              _id: new mongoose.Types.ObjectId(req.user._id)
+            _id: new mongoose.Types.ObjectId(req.user._id)
           }
       },
       {
